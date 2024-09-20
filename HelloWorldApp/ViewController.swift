e2c335216bf1973c7b6db7e14b13bb37ccda4248
Farukh Iskalinov <fiskalinov@one.kz>
2024-09-20 10:14:29 +0500

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by FIskalinov on 20.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
